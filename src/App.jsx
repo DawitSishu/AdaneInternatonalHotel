6ff2de9cb4ef0117eca8e6887471b18221907028
DawitSishu <dawitalemayhu55@gmail.com>
2023-07-12 04:01:45 -0400

    home page with loader

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,7 @@ function App() {
 
   return (
     <div >
-      <Loader />
-      {/* <NavBar />
-      <Home /> */}
+        <Home />
     </div>
   )
 }
--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,15 +1,48 @@
-import background from '../../assets/homevid.mp4';
+import React, { useEffect, useRef,useState, Suspense } from 'react';
+import background from '../../assets/ff.mp4'; 
 import './style.css';
+import Loader from '../Loader';
 
-const Home = (props) => {
+
+const Home = () => {
+  const videoRef = useRef(null);
+  const [isMuted, setIsMuted] = useState(true);
+
+  useEffect(() => {
+    const videoElement = videoRef.current;
+
+    const handleVideoLoaded = () => {
+      videoElement.play().catch((error) => {
+        console.error('Video playback failed:', error);
+      });
+    };
+
+    videoElement.addEventListener('loadeddata', handleVideoLoaded);
+
+    return () => {
+      videoElement.removeEventListener('loadeddata', handleVideoLoaded);
+    };
+  }, []);
+
+  const toggleAudio = () => {
+    const videoElement = videoRef.current;
+    videoElement.muted = !videoElement.muted;
+    setIsMuted(!isMuted);
+  };
+
   return (
-    <div>
-    <div className="main panel">
-        <video src={background} autoPlay muted  loop/>
-        <div className="overlay"></div>
-        {/* <HomeBody /> */}
+    <Suspense fallback={<Loader />}>
+      <div className="main panel">
+        <video ref={videoRef} src={background} autoPlay loop muted={isMuted} />
+        <div className="overlay">
+          <button onClick={toggleAudio}>
+            {isMuted ? 'Unmute' : 'Mute'} Audio
+          </button>
         </div>
-    </div>
-  )
-}
+      </div>
+    </Suspense>
+  );
+};
+
 export default Home;
+
