ddc4c588f1f361299fd1a3763c61c2f87384636d
DawitSishu <dawitalemayhu55@gmail.com>
2023-07-20 15:35:03 -0400

    Rooms implemented

--- src/Components/Services/Room.jsx
+++ src/Components/Services/Room.jsx
@@ -1,25 +1,24 @@
 import React, { useState } from 'react';
-import { Grid, Typography, IconButton, List, ListItem, ListItemIcon } from '@mui/material';
+import { Grid, Typography, IconButton, List, ListItem, ListItemIcon, Box } from '@mui/material';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import StarIcon from '@mui/icons-material/Star';
 import h from '../../assets/1.jpg';
 import j from '../../assets/2.jpg';
 
-const RoomPage = ({}) => {
+const RoomPage = ({roomData}) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   // Replace these with your actual image URLs
 //   const mainImage = h;
-  const thumbnailImages = [h,j,h,j];
 
    const handleNextImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % thumbnailImages.length);
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % roomData.images.length);
   };
 
   const handlePreviousImage = () => {
     setCurrentImageIndex((prevIndex) =>
-      prevIndex === 0 ? thumbnailImages.length - 1 : prevIndex - 1
+      prevIndex === 0 ? roomData.images.length - 1 : prevIndex - 1
     );
   };
 
@@ -28,14 +27,15 @@ const RoomPage = ({}) => {
   };
 
   return (
-    <Grid container justifyContent="center">
+    <Box sx={{padding:3,margin:3}}>
+    <Grid container justifyContent="center" >
         
       <Grid item xs={12} md={8}>
         {/* Main Image */}
         
         <Grid  sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <img
-          src={thumbnailImages[currentImageIndex]}
+          src={roomData.images[currentImageIndex]}
           alt="Main"
           style={{
             maxWidth: '100%',
@@ -48,10 +48,10 @@ const RoomPage = ({}) => {
         <Grid container alignItems="center" justifyContent="center" sx={{ mt: 2 }}>
           <Grid item>
             <IconButton onClick={handlePreviousImage}>
-              <NavigateBeforeIcon />
+              <NavigateBeforeIcon color='primary' />
             </IconButton>
           </Grid>
-          {thumbnailImages.map((thumbnail, index) => (
+          {roomData.images.map((thumbnail, index) => (
            <Grid item key={index} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
            <img
              src={thumbnail}
@@ -69,19 +69,19 @@ const RoomPage = ({}) => {
           ))}
           <Grid item>
             <IconButton onClick={handleNextImage}>
-              <NavigateNextIcon />
+              <NavigateNextIcon color='primary' />
             </IconButton>
           </Grid>
         </Grid>
       </Grid>
       <Grid item xs={12} md={4} sx={{ mt: 2, pl: { md: 2 } }}>
         {/* Title */}
-        <Typography variant="h4" align="center">
-          Room Title
+        <Typography variant="h4" color='primary'>
+          {roomData.title}
         </Typography>
 
         {/* Description */}
-        <List sx={{ mt: 2 }}>
+        {/* <List sx={{ mt: 2 }}>
           <ListItem>
             <ListItemIcon>
               <StarIcon />
@@ -99,11 +99,22 @@ const RoomPage = ({}) => {
               <StarIcon />
             </ListItemIcon>
             <Typography>Room feature 3</Typography>
-          </ListItem>
+          </ListItem> */}
+          <List sx={{ mt: 2 }}>
+             {roomData.features.map((feature, index) => (
+              <ListItem key={index}>
+                <ListItemIcon>
+                  <StarIcon color='primary' />
+                </ListItemIcon>
+                <Typography>{feature}</Typography>
+              </ListItem>
+            ))}
+          </List>
           {/* Add more room features here */}
-        </List>
+        {/* </List> */}
       </Grid>
     </Grid>
+    </Box>
   );
 };
 
--- src/Components/Services/index.jsx
+++ src/Components/Services/index.jsx
@@ -1,15 +1,100 @@
-import { Grid, Typography } from "@mui/material";
+import React, { useState, useRef } from "react";
+import {
+  Grid,
+  Box,
+  Typography,
+  Card,
+  CardMedia,
+  CardContent,
+  Button,
+  CardActions,
+} from "@mui/material";
 import backgroundImage from "../../assets/h.jpg";
 import "./style.css";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
 import RoomPage from "./Room";
+import h from "../../assets/1.jpg";
+import j from "../../assets/2.jpg";
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#FFD700",
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 768,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+});
 
 // room
+const roomData = [
+  {
+    image: h,
+    description: "Description of Room 1...",
+    images: [h, j, h, j, h],
+    title: "Room 1",
+    features: [
+      "Room 1 feature 1",
+      "Room 1 feature 2",
+      "Room 1 feature 3",
+      // Add more features for Room 1
+    ],
+  },
+  {
+    image: j,
+    description: "Description of Room 2...",
+    images: [h, j, h, j, h],
+    title: "Room ",
+    features: ["Room 2 feature 1", "Room 2 feature 2", "Room 2 feature 3"],
+  },
+  {
+    image: h,
+    description: "Description of Room 1...",
+    images: [h, j, h, j, h],
+    title: "Room 1",
+    features: [
+      "Room 1 feature 1",
+      "Room 1 feature 2",
+      "Room 1 feature 3",
+      // Add more features for Room 1
+    ],
+  },
+  {
+    image: j,
+    description: "Description of Room 2...",
+    images: [h, j, h, j, h],
+    title: "Room ",
+    features: ["Room 2 feature 1", "Room 2 feature 2", "Room 2 feature 3"],
+  },
+];
+
 const index = () => {
+  const [showRoomPage, setShowRoomPage] = useState(false);
+  const [idx, setIdx] = useState(0);
+  const scrollToRef = useRef(null);
+
+  const handleLoadMore = (i) => {
+    setShowRoomPage(true);
+    setIdx(i);
+    scrollToRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <div>
       <Navbar />
+      <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
       <Grid
         container
         spacing={0}
@@ -37,21 +122,94 @@ const index = () => {
             Personalized Relaxation.
           </Typography>
         </Grid>
-        <Grid container justifyContent="center">
-      </Grid>
+        <Grid container justifyContent="center"></Grid>
       </Grid>
       <Grid item justifyContent="center" sx={{ padding: 3 }}>
-          <Typography variant="p" align="center">
-            Whether you're planning a romantic wedding or a special event, a
-            family vacation, or a solo adventure, our hotel offers a range of
-            special packages that are tailored to meet your needs. From wedding
-            packages that offer the perfect setting for your special day, to
-            relaxation packages that allow you to unwind and recharge, we have
-            something for everyone. Our packages include exclusive benefits such
-            as discounted rates, complimentary meals, and access to our top
-            amenities. Browse our selection of packages today and discover the
-            perfect experience for you.
-          </Typography>
+        <Typography variant="p" align="center">
+          Whether you're planning a romantic wedding or a special event, a
+          family vacation, or a solo adventure, our hotel offers a range of
+          special packages that are tailored to meet your needs. From wedding
+          packages that offer the perfect setting for your special day, to
+          relaxation packages that allow you to unwind and recharge, we have
+          something for everyone. Our packages include exclusive benefits such
+          as discounted rates, complimentary meals, and access to our top
+          amenities. Browse our selection of packages today and discover the
+          perfect experience for you.
+        </Typography>
+      </Grid>
+      <Grid container justifyContent="center" spacing={2} sx={{margin:3}}>
+          <Grid item xs={12}>
+            <Typography variant="h3" align="center" color="primary">
+              Comfortable Rooms
+            </Typography>
+          </Grid>
+          <Grid container justifyContent="center">
+            <Box
+              sx={{
+                width: "60%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Typography variant="body2" align="center">
+                Our rooms are designed for your comfort and relaxation. We
+                provide comfortable bedding and a peaceful ambiance to help you
+                unwind and get a good night's sleep. You'll also have access to
+                modern amenities, such as high-speed internet, flat-screen TVs,
+                and complimentary beverages. Whether you're traveling alone or
+                with family and friends, we have a range of room types to choose
+                from, including suites and connecting rooms.
+              </Typography>
+            </Box>
+          </Grid>
+        </Grid>
+        <Grid container justifyContent="center" spacing={4}>
+          {roomData.map((card, index) => (
+            <Grid item key={index}>
+              <Card
+                sx={{
+                  maxWidth: 345,
+                  minHeight: 380,
+                  marginBottom: 5,
+                  boxShadow: "0 2px 10px 0 rgba(255, 215, 0, 0.5)",
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={card.image}
+                  alt={card.title}
+                />
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                    sx={{ color: "gold", fontWeight: "bold" }}
+                  >
+                    {card.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {card.description}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    fullWidth
+                    onClick={() => handleLoadMore(index)}
+                  >
+                    Show More
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+        <Grid ref={scrollToRef}>
+          {showRoomPage ? <RoomPage roomData={roomData[idx]} /> : null}
         </Grid>
       <Grid container spacing={2} justifyContent="center">
         <Grid item xs={12} md={6}>
@@ -69,6 +227,7 @@ const index = () => {
               variant="h3"
               align="center"
               className="about-text-title"
+              color="primary"
             >
               Restaurant
             </Typography>
@@ -138,6 +297,7 @@ const index = () => {
               variant="h3"
               align="center"
               className="about-text-title"
+              color="primary"
             >
               Bar
             </Typography>
@@ -172,6 +332,7 @@ const index = () => {
               variant="h3"
               align="center"
               className="about-text-title"
+              color="primary"
             >
               Conference and Banquet Facilities
             </Typography>
@@ -243,14 +404,15 @@ const index = () => {
               variant="h3"
               align="center"
               className="about-text-title"
+              color="primary"
             >
               Reception and Security
             </Typography>
             <Typography variant="body1" align="center">
-              Our hotel offers 24-hour reception and security service,
-              ensuring that guests have access to assistance and support around
-              the clock. The reception staff are always available to help with
-              any needs or requests, whether it is checking in or out, arranging
+              Our hotel offers 24-hour reception and security service, ensuring
+              that guests have access to assistance and support around the
+              clock. The reception staff are always available to help with any
+              needs or requests, whether it is checking in or out, arranging
               transportation, or providing recommendations for local
               attractions. Additionally, the hotel has a dedicated security team
               that is available at all times to ensure the safety and security
@@ -258,12 +420,13 @@ const index = () => {
               systems and trained personnel, guests can be assured of a safe and
               secure stay. The combination of round-the-clock reception and
               security services provides guests with peace of mind and a
-              worry-free experience during their stay at Adane International Hotel.
+              worry-free experience during their stay at Adane International
+              Hotel.
             </Typography>
           </Grid>
         </Grid>
-        <RoomPage />
       </Grid>
+      </ThemeProvider>
       <Footer />
     </div>
   );
