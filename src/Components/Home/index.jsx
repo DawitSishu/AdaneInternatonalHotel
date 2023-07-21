c9c8fb32e68ae48fbe09d271be79ea35cc606c91
DawitSishu <dawitalemayhu55@gmail.com>
2023-07-21 02:57:37 -0400

    home fixes

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -5,6 +5,26 @@ import { VolumeOff, VolumeUp } from "@mui/icons-material";
 import background from "../../assets/homevid.mp4";
 import "./style.css";
 import Loader from "../Loader";
+import Logo from "../../assets/logo.png";
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+      primary: {
+        main: "#FFD700",
+      },
+    },
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 768, 
+        md: 960,
+        lg: 1280,
+        xl: 1920,
+      },
+    },
+  });
+
 
 const Home = ({stop}) => {
   const videoRef = useRef(null);
@@ -34,77 +54,73 @@ const Home = ({stop}) => {
 
   return (
     <Suspense fallback={<Loader />}>
-      <div className="main">
-        <video ref={videoRef} src={background} autoPlay  muted={isMuted} onEnded={stop} />
-        <div className="overlay">
-        <IconButton onClick={toggleAudio} style={{
-        position: "fixed",
-        bottom: "20px",
-        right: "20px",
-        zIndex: 99,
-        fontSize: "32px",
-
-      }}   >
-        {isMuted ? (
-        <VolumeOff style={{ borderRadius: "50%", border: "2px solid #fff", color: "#fff" }} />
-      ) : (
-        <VolumeUp style={{ borderRadius: "50%", border: "2px solid #fff", color: "#fff" }} />
-      )}
-    </IconButton>
-        </div>
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            height: "100%",
+      <div className="main" style={{height:'100vh',overflowY:'hidden'}}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+      <video ref={videoRef} src={background} autoPlay muted={isMuted} onEnded={stop} />
+      <div className="overlay">
+        <IconButton
+          onClick={toggleAudio}
+          style={{
+            position: 'fixed',
+            bottom: '20px',
+            right: '20px',
+            zIndex: 99,
+            fontSize: '32px',
           }}
         >
-          <Grid container justifyContent="center">
-            <Grid item xs={12}>
-              <Typography
-                variant="h3"
-                sx={{ fontFamily: "myCursive", color: "white" }}
-                align="center"
-              >
-                Adane
-              </Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <Typography
-                variant="h3"
-                sx={{ fontFamily: "myCursive", color: "white" }}
-                align="center"
-                xs={12}
-              >
-                International
-              </Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <Typography
-                variant="h3"
-                sx={{ fontFamily: "myCursive", color: "white" }}
-                align="center"
-                xs={12}
-              >
-                Hotel
-              </Typography>
-            </Grid>
-        <Button
-      variant="outlined"
-      style={{ backgroundColor: "transparent",borderRadius: "20px", border: "1px solid #fff", color: "#fff",position: "fixed",
-      bottom: "20px",
-      zIndex: 99, }}
-      onClick={stop}
-    >
-      Skip & continue
-    </Button>
-          </Grid>
-        </Box>
+          {isMuted ? (
+            <VolumeOff style={{ borderRadius: '50%', border: '2px solid gold', color: 'gold' }} />
+          ) : (
+            <VolumeUp style={{ borderRadius: '50%', border: '2px solid gold', color: 'gold' }} />
+          )}
+        </IconButton>
       </div>
+        {/* Responsive Image */}
+        
+        <Grid container justifyContent="center">
+        <Box sx={{ width: '100%', maxWidth: '500px',position: 'fixed',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            zIndex: 99,
+            fontSize: '32px',}}>
+          <img src={Logo} alt="Your Image" style={{ width: '100%', height: 'auto' }} />
+        </Box>
+          {/* <Grid item xs={12}>
+            <Typography variant="h3" sx={{ fontFamily: 'myCursive', color: 'white' }} align="center">
+              Adane
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="h3" sx={{ fontFamily: 'myCursive', color: 'white' }} align="center" xs={12}>
+              International
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="h3" sx={{ fontFamily: 'myCursive', color: 'white' }} align="center" xs={12}>
+              Hotel
+            </Typography>
+          </Grid> */}
+          <Button
+            variant="outlined"
+            style={{
+              backgroundColor: 'transparent',
+              borderRadius: '20px',
+              border: '1px solid gold',
+              color: 'gold',
+              position: 'fixed',
+              bottom: '20px',
+              zIndex: 99,
+            }}
+            onClick={stop}
+          >
+            Skip & continue
+          </Button>
+        </Grid>
+      {/* </Box> */}
+      </ThemeProvider>
+    </div>
     </Suspense>
   );
 };
