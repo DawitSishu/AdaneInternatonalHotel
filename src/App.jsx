3ffa13911588baa96325626bd7c26c0fcbe35949
DawitSishu <dawitalemayhu55@gmail.com>
2023-07-21 03:26:46 -0400

    loader implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,84 @@
-import {useState,Suspense} from 'react';
-import './App.css';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-// import NavBar from './Components/Navbar';
-import Home from './Components/Home';
-import Loader from './Components/Loader';
-import About from './Components/About';
-// import Footer from './Components/Footer';
-import Home2 from './Components/Home2';
-import Gallery from './Components/Gallery';
-import Contact from './Components/Contact';
-import Special from './Components/Special';
-import Services from './Components/Services';
+import React, { useState, Suspense } from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Loader from "./Components/Loader";
+
+const Home = React.lazy(() => import("./Components/Home"));
+const Home2 = React.lazy(() => import("./Components/Home2"));
+const About = React.lazy(() => import("./Components/About"));
+const Gallery = React.lazy(() => import("./Components/Gallery"));
+const Services = React.lazy(() => import("./Components/Services"));
+const Special = React.lazy(() => import("./Components/Special"));
+const Contact = React.lazy(() => import("./Components/Contact"));
 
 function App() {
-  const [vidplaying,setVidPlaying] = useState(true);
+  const [vidplaying, setVidPlaying] = useState(true);
 
   const stopVideo = () => {
     setVidPlaying(false);
-  }
- {/* {
-         vidplaying ?  <Home stop={stopVideo}/> : <About />
-       } */}
+  };
+
   return (
     <BrowserRouter>
-    <Routes>
-        <Route exact path="/" element={ vidplaying ?  <Home stop={stopVideo}/> : <Home2 />}/>
-        <Route exact path="/about" element={ <About />}/>
-        <Route exact path="/gallery" element={ <Gallery />}/>
-        <Route exact path="/services" element={ <Services />}/>
-        <Route exact path="/special-offers" element={ <Special />}/>
-        <Route exact path="/Contact" element={ <Contact />}/>
-    </Routes>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <Suspense fallback={<Loader />}>
+              {vidplaying ? <Home stop={stopVideo} /> : <Home2 />}
+            </Suspense>
+          }
+        />
+        <Route
+          exact
+          path="/about"
+          element={
+            <Suspense fallback={<Loader />}>
+              <About />
+            </Suspense>
+          }
+        />
+        <Route
+          exact
+          path="/gallery"
+          element={
+            <Suspense fallback={<Loader />}>
+              <Gallery />
+            </Suspense>
+          }
+        />
+        <Route
+          exact
+          path="/services"
+          element={
+            <Suspense fallback={<Loader />}>
+              <Services />
+            </Suspense>
+          }
+        />
+        <Route
+          exact
+          path="/special-offers"
+          element={
+            <Suspense fallback={<Loader />}>
+              <Special />
+            </Suspense>
+          }
+        />
+        <Route
+          exact
+          path="/Contact"
+          element={
+            <Suspense fallback={<Loader />}>
+              <Contact />
+            </Suspense>
+          }
+        />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Loader/index.jsx
+++ src/Components/Loader/index.jsx
@@ -4,15 +4,6 @@ import "./style.css";
 const index = () => {
   return (
     <div className="main-loader">
-      {/* <div className="pyramid-loader">
-        <div className="wrapper">
-          <span className="side side1"></span>
-          <span className="side side2"></span>
-          <span className="side side3"></span>
-          <span className="side side4"></span>
-          <span className="shadow"></span>
-        </div>
-      </div> */}
       <div className="cube-loader">
         <div className="cube-top"></div>
         <div className="cube-wrapper">
