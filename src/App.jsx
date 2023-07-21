20def65d7c43e9aa65285e86aa61c18bed23aec0
DawitSishu <dawitalemayhu55@gmail.com>
2023-07-21 00:42:25 -0400

    browser routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import {useState,Suspense} from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 // import NavBar from './Components/Navbar';
-// import Home from './Components/Home';
+import Home from './Components/Home';
 import Loader from './Components/Loader';
 import About from './Components/About';
 // import Footer from './Components/Footer';
@@ -10,7 +11,6 @@ import Gallery from './Components/Gallery';
 import Contact from './Components/Contact';
 import Special from './Components/Special';
 import Services from './Components/Services';
-import RoomPage from './Components/Services/Room';
 
 function App() {
   const [vidplaying,setVidPlaying] = useState(true);
@@ -18,25 +18,20 @@ function App() {
   const stopVideo = () => {
     setVidPlaying(false);
   }
-
-  return (
-    // <div >
-    <Suspense fallback={<Loader />}>
-
-       {/* {
+ {/* {
          vidplaying ?  <Home stop={stopVideo}/> : <About />
        } */}
-         {/* <Home /> */}
-         {/* <Loader /> */}
-          {/* <NavBar /> */}
-         {/* <About /> */}
-         {/* <Home2 /> */}
-         {/* <Gallery /> */}
-         {/* <Contact /> */}
-        {/* <Special /> */}
-        <Services />
-        {/* <RoomPage /> */}
-    </Suspense>
+  return (
+    <BrowserRouter>
+    <Routes>
+        <Route exact path="/" element={ vidplaying ?  <Home stop={stopVideo}/> : <Home2 />}/>
+        <Route exact path="/about" element={ <About />}/>
+        <Route exact path="/gallery" element={ <Gallery />}/>
+        <Route exact path="/services" element={ <Services />}/>
+        <Route exact path="/special-offers" element={ <Special />}/>
+        <Route exact path="/Contact" element={ <Contact />}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
